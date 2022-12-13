5bf883d5156f5e76d5c260bd27d358021964183b
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-12-13 12:44:09 +0100

    eyouuu

---
+++ Ejercicio1.py
@@ -0,0 +1,26 @@
+"""""
+¡Empiezas a trabajar para una nueva empresa elegante que espera revolucionar las redes
+sociales! ¡JADEAR! Tuvieron esta gran idea de que los usuarios deberían poder especificar
+palabras clave relevantes para sus publicaciones utilizando una idea ingeniosa al anteponer
+esas palabras clave con el signo de libra (#). Su trabajo es extraer esas palabras clave para que
+puedan usarse más tarde para cualquier propósito.
+Nota:
+• Los signos de libra por sí solos no cuentan, por ejemplo: la cadena "#" devolvería una
+matriz vacía.
+• Si una palabra está precedida por más de un hashtag, solo cuenta el último hashtag
+(por ejemplo, "##alot" devolvería ["alot"])
+• Los hashtags no pueden estar en medio de una palabra (por ejemplo, "hashtag en
+#línea" devuelve una matriz vacía)
+• Los hashtags deben preceder a los caracteres alfabéticos (por ejemplo, "#120398" o
+"#?" no son válidos)
+Entrada: cadena de palabras, donde algunas palabras pueden contener un hashtag.
+Salida: matriz de cadenas que tenían el prefijo del hashtag, pero que no contienen el hashtag.
+"""
+def Matriz_de_Hashtag(string):
+    #Si cadena viene precedidda de un # Coger dicha cadena. Si elemento tiene la forma: #String return String, o por ej: tiene la forma 
+    #: #Str#ing, return [str, ing]
+    matriz = []
+    if string == ("#", string):
+        return(matriz.append(string))
+Cadena = "#hello" 
+print(Matriz_de_Hashtag(Cadena))
---
+++ Ejercicio4.py
@@ -0,0 +1,32 @@
+"""""
+En algunos países de la antigua Unión Soviética existía la creencia de los boletos de la
+suerte. Se creía que un billete de transporte de cualquier tipo traía suerte si la suma de los
+dígitos de la mitad izquierda de su número era igual a la suma de los dígitos de la mitad
+derecha. Aquí hay ejemplos de tales números:
+003111 # 3 = 1 + 1 + 1
+813372 # 8 + 1 + 3 = 3 + 7 + 2
+17935 # 1 + 7 = 3 + 5 // si la longitud es impar, debes ignorar el número del medio al
+sumar las mitades.
+56328116 # 5 + 6 + 3 + 2 = 8 + 1 + 1 + 6
+Dichos boletos se comían después de usarlos o se recolectaban para fanfarronear.
+Su tarea es escribir una función luck_check(str), que devuelve true/Truesi el argumento es una
+representación decimal de cadena de un número de boleto de la suerte, o false/Falsepara
+todos los demás números. Debería arrojar errores para cadenas vacías o cadenas que no
+representan un número decimal.
+"""
+def luck_check(string):
+    #Si la longitud de la cadena es par, sumar los dos primeros elementos de la cadena y los dos ultimos elementos de la cadena
+    #Si la longitud de la cadena es impar, sumar los dos primeros elementos de la cadena y los dos ultimos elementos de la cadena
+    #Si la suma de los dos primeros elementos de la cadena es igual a la suma de los dos ultimos elementos de la cadena, return True
+    #Si la suma de los dos primeros elementos de la cadena no es igual a la suma de los dos ultimos elementos de la cadena, return False
+    if len(string) % 2 == 0:
+        if string[0] + string[1] == string[-2] + string[-1]:
+            return True
+        else:
+            return False
+    else:
+        if string[0] + string[1] == string[-2] + string[-1]:
+            return True
+        else:
+            return False
+print(luck_check("813372"))
