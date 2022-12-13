c8c057658ba1a136a80cbe57cf5cad55144d0040
rioperezg <113980673+rioperezg@users.noreply.github.com>
2022-12-13 12:43:53 +0100

    eyouu

---
+++ Ejercicio2.py
@@ -0,0 +1,18 @@
+"""""
+El malvado gobierno de programación ha prohibido el uso de números. Su tarea, si elige
+aceptarla, es devolver números, sin usar números.
+Problemas:
+• No puede usar literales numéricos en su código fuente.
+• No puede usar la propiedad de longitud directamente en su código.
+Meta:
+• Tienes que devolver 'Puedo escribir números como 1, 2, 3'.
+"""
+def Numero(string):
+    #Convertir cadena en int
+    
+    
+       
+            
+
+
+    
---
+++ Ejercicio5.py
@@ -0,0 +1,19 @@
+"""""
+Escriba una rotate que gire una matriz bidimensional (una matriz) ya sea en sentido horario o
+antihorario 90 grados y devuelva la matriz rotada.
+La función acepta dos parámetros: una matriz y una cadena que especifica la dirección o
+rotación. La dirección será "clockwise"o "counter-clockwise".
+Aquí hay un ejemplo de cómo se usará su función:
+var matrix = [[1, 2, 3],
+ [4, 5, 6],
+ [7, 8, 9]];
+rotate(matrix, "clockwise"); // Would return [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+Para ayudarlo a visualizar la matriz rotada, aquí está formateada como una cuadrícula:
+[[7, 4, 1],
+ [8, 5, 2],
+ [9, 6, 3]]
+Girado en sentido contrario a las agujas del reloj se vería así:
+[[3, 6, 9],
+ [2, 5, 8],
+ [1, 4, 7]]
+ """
